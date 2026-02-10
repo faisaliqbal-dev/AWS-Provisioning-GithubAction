f1c38dde3d4e14e732eedb01a81ff000e1918ce5
faisal <mdfaisaliqbal4257@gmail.com>
2026-02-10 23:28:26 +0530

    updated some files

---
+++ terraform.tfvars
@@ -0,0 +1,5 @@
+instance_type = "t3.micro"
+ami = "ami-0ba8d27d35e9915fb"
+security_groups = "sg-0676e13965795fc09"
+instance_name = "Practice-VM"
+count = 1
---
+++ varibale.tf
@@ -0,0 +1,17 @@
+variable "ami" {
+    type = string
+  
+}
+variable "security_groups" {
+   type = string
+}
+variable "instance_type" {
+    type = string 
+}
+variable "instance_name" {
+    type = string
+}
+variable "count" {
+    type = number
+  
+}
